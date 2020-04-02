1667988033b6f172eb1143956624092e97c85df1
Gabriel Fardin <gsfardin@gmail.com>
2020-04-01 23:35:23 -0300

    Tratamento de exceção nativo

---
+++ src/modelo/entities/Account.java
@@ -0,0 +1,68 @@
+package modelo.entities;
+
+public class Account {
+
+	private Integer number;
+	private String holder;
+	private double balance;
+	private double withdrawLimit;
+	
+	public Account(Integer number, String holder, double balance, double withdrawLimit) {
+		this.number = number;
+		this.holder = holder;
+		this.balance = balance;
+		this.withdrawLimit = withdrawLimit;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+
+	public String getHolder() {
+		return holder;
+	}
+
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public double getWithdrawLimit() {
+		return withdrawLimit;
+	}
+
+	public void setWithdrawLimit(double withdrawLimit) {
+		this.withdrawLimit = withdrawLimit;
+	}
+	
+	public void deposit(double amount) {
+		balance += amount;
+	}
+	
+	public void withdraw(double amount) throws IllegalArgumentException {
+		
+		if(amount > withdrawLimit) {
+			throw new IllegalArgumentException("Withdraw error: The amount exceeds withdraw limit.");
+		}
+		
+		if(amount > balance) {
+			throw new IllegalArgumentException("Withdraw error: Not enough balance.");
+		}
+		
+		balance -= amount;
+	}
+
+	@Override
+	public String toString() {
+		return "New balance: " + balance;
+	}
+	
+	
+}
